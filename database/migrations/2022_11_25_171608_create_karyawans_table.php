60677006eeb60fcebb0a829dc8e92fb6b53b73ca
mhmmdgbrnn <itua275@gmail.com>
2022-11-25 10:14:38 -0800

    relasi role dengan user

---
+++ database/migrations/2022_11_25_171608_create_karyawans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKaryawansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('karyawans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('hp')->length('13');
+            $table->string('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('karyawans');
+    }
+}
